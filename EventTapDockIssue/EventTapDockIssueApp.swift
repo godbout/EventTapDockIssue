fbfaf283f472bd049b9c244fa8126f9560f7380f
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2023-08-19 15:30:08 +0800

    Initial Commit

---
+++ EventTapDockIssue/EventTapDockIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventTapDockIssueApp.swift
+//  EventTapDockIssue
+//
+//  Created by G. on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct EventTapDockIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
